edeefbf7ef9b15ced519dc4843ea0ea850770bda
cesar <cruzatadelacruzc@gmail.com>
2021-02-09 01:01:47 -0500

    Create IndexMapper

---
+++ src/main/java/cu/sld/ucmgt/directory/service/mapper/IndexMapper.java
@@ -0,0 +1,16 @@
+package cu.sld.ucmgt.directory.service.mapper;
+
+import java.util.Set;
+
+/**
+ * Contract for a generic dto to entity mapper.
+ *
+ * @param <I> - index type parameter.
+ * @param <E> - domain type parameter.
+ */
+public interface IndexMapper<I, E> {
+
+    I toIndex(E entity);
+
+    Set<I> toIndexes(Set<E> entities);
+}
