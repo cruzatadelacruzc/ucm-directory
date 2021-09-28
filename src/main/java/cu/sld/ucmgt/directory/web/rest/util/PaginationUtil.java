ea64ea5c63f3838f7f3785069ebf913c73d4eb7d
cesar <cruzatadelacruzc@gmail.com>
2021-09-28 19:08:54 -0400

    Add response headers

--- src/main/java/cu/sld/ucmgt/directory/web/rest/util/PaginationUtil.java
+++ src/main/java/cu/sld/ucmgt/directory/web/rest/util/PaginationUtil.java
@@ -10,6 +10,10 @@ import java.text.MessageFormat;
 public final class PaginationUtil {
 
     private static final String HEADER_X_TOTAL_COUNT = "X-Total-Count";
+    private static final String HEADER_X_SORT = "X-Sort";
+    private static final String HEADER_X_SIZE = "X-Size";
+    private static final String HEADER_X_PAGE = "X-Page";
+    private static final String HEADER_X_PAGEABLE = "X-Pageable";
 
     public PaginationUtil() {
     }
@@ -17,6 +21,10 @@ public final class PaginationUtil {
     public static <T> HttpHeaders generatePaginationHeaders(UriComponentsBuilder builder, Page<T> page) {
         HttpHeaders headers = new HttpHeaders();
         headers.add(HEADER_X_TOTAL_COUNT,Long.toString(page.getTotalElements()));
+        headers.add(HEADER_X_SORT,page.getSort().toString());
+        headers.add(HEADER_X_SIZE,Long.toString(page.getSize()));
+        headers.add(HEADER_X_PAGE,Long.toString(page.getNumber()));
+        headers.add(HEADER_X_PAGEABLE, String.valueOf(page.getPageable().isPaged()));
 
         int pageNumber = page.getNumber();
         int pageSize = page.getSize();
