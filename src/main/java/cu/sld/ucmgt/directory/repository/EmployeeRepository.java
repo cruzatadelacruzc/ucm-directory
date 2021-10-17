f18a2cd4b98c0b97133a1278f302d89165f46b5c
cesar <cruzatadelacruzc@gmail.com>
2021-10-17 11:02:01 -0400

    Fixed attribute paths

--- src/main/java/cu/sld/ucmgt/directory/repository/EmployeeRepository.java
+++ src/main/java/cu/sld/ucmgt/directory/repository/EmployeeRepository.java
@@ -3,24 +3,15 @@ package cu.sld.ucmgt.directory.repository;
 import cu.sld.ucmgt.directory.domain.Employee;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+public interface EmployeeRepository extends JpaRepository<Employee, UUID>, JpaSpecificationExecutor<Employee> {
 
-    @EntityGraph(attributePaths = {
-            "charge",
-            "phones",
-            "category",
-            "district",
-            "specialty",
-            "workPlace",
-            "profession",
-            "scientificDegree",
-            "teachingCategory"
-    })
+    @EntityGraph(attributePaths = {"phones"})
     Optional<Employee> findEmployeeWithAssociationsById(UUID uuid);
 }
