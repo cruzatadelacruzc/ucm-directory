8e50943cd5bb1d7edafbaeabd92bcdea71cbde1e
cesar <cruzatadelacruzc@gmail.com>
2021-10-18 17:50:07 -0400

    Modified Criteria doc

--- src/main/java/cu/sld/ucmgt/directory/service/criteria/NomenclatureCriteria.java
+++ src/main/java/cu/sld/ucmgt/directory/service/criteria/NomenclatureCriteria.java
@@ -15,7 +15,7 @@ import java.util.Objects;
  * in {@link NomenclatureResource} to receive all the possible filtering options from
  * the Http GET request parameters.
  * For example the following could be a valid request:
- * {@code /nomenclatures?id.greaterThan=5&attr1.contains=something&attr2.specified=false}
+ * {@code /nomenclatures/or?id.greaterThan=5&attr1.contains=something&attr2.specified=false}
  * As Spring is unable to properly convert the types, unless specific {@link Filter} class are used, we need to use
  * fix type specific filters.
  */
