805c0747ef45179ddd5cb9c696b18e433b6b7995
cesar <cruzatadelacruzc@gmail.com>
2022-02-09 06:10:01 -0500

    Modified behavior data recollection of gender & birthdate

--- src/main/java/cu/sld/ucmgt/directory/service/mapper/EmployeeMapper.java
+++ src/main/java/cu/sld/ucmgt/directory/service/mapper/EmployeeMapper.java
@@ -1,9 +1,15 @@
 package cu.sld.ucmgt.directory.service.mapper;
 
 import cu.sld.ucmgt.directory.domain.Employee;
+import cu.sld.ucmgt.directory.domain.Gender;
 import cu.sld.ucmgt.directory.service.dto.EmployeeDTO;
 import org.mapstruct.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoField;
 import java.util.UUID;
 
 /**
@@ -65,5 +71,34 @@ public interface EmployeeMapper extends EntityMapper<EmployeeDTO, Employee> {
         Employee employee = new Employee();
         employee.setId(id);
         return employee;
+    }
+    @AfterMapping
+    default void setBirthDateFromCI(EmployeeDTO dto  , @MappingTarget Employee entity) {
+        if (dto.getCi() != null && dto.getCi().length() == 11 && dto.getBirthdate() == null) {
+            try {
+//                String date = String.format("%s%s%s", dto.getCi().substring(0, 2), dto.getCi().substring(2, 4), dto.getCi().substring(4, 6));
+//                SimpleDateFormat dateFormat = new SimpleDateFormat("yyMMdd");
+//                LocalDate birthdate = dateFormat.parse(date).toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            String date1 = String.format("%s%s%s", dto.getCi().substring(4, 6), dto.getCi().substring(2, 4), dto.getCi().substring(0, 2));
+            int era = Integer.parseInt(dto.getCi().substring(6,7));
+            DateTimeFormatter format = new DateTimeFormatterBuilder()
+                    .appendPattern("ddMM")
+                    .appendValueReduced(ChronoField.YEAR, 2, 2, era <= 5 ? 1900: 2000)
+                    .toFormatter();
+                entity.setBirthdate(LocalDate.parse(date1, format));
+            } catch (DateTimeParseException e) {
+                entity.setBirthdate(LocalDate.now());
+            }
+        }
+
+    }
+
+    @AfterMapping
+    default void setGenderFromCI(EmployeeDTO dto  , @MappingTarget Employee entity) {
+        if (dto.getCi() != null && dto.getCi().length() == 11 && dto.getGender() == null) {
+            String genderDigit = dto.getCi().substring(9,10);
+           Gender gender = Integer.parseInt(genderDigit) %2 == 0 ? Gender.Masculino: Gender.Femenino;
+           entity.setGender(gender);
+        }
     }
 }
